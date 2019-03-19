018a3c438e577b4bf71dbe346015512362c232e8
chenzhiwei0109 <1138476755@qq.com>
2019-03-19 10:43:12 +0800

    commit

--- src/pages/Project/Add.js
+++ src/pages/Project/Add.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import AddComponent from '@/components/AddComponent/AddComponent'
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';//面包屑
 export default class Add extends Component {
   render() {
     return (
-      <div>
-        <h2>项目编辑</h2>
+      <PageHeaderWrapper title="项目编辑">
+        <div>
         <AddComponent/>
-      </div>
+        </div>
+      </PageHeaderWrapper>
+
     )
   }
 }
