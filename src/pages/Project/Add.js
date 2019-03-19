842baaca9f99aa4c4bfef4e396692a687e6aa36c
tyler <1732749825@qq.com>
2019-03-19 09:04:41 +0800

    lxj

--- src/pages/Project/Add.js
+++ src/pages/Project/Add.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import AddComponent from '@/components/AddComponent/AddComponent'
 export default class Add extends Component {
   render() {
     return (
       <div>
         <h2>项目编辑</h2>
+        <AddComponent/>
       </div>
     )
   }
