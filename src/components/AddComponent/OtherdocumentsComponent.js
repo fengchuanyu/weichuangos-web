6ffd9233f8e06b5e8b3d55de77b4774ffd5178d2
Administrator <1016712148@qq.com>
2019-03-20 14:43:55 +0800

    commit

--- src/components/AddComponent/OtherdocumentsComponent.js
+++ src/components/AddComponent/OtherdocumentsComponent.js
@@ -1,11 +1,37 @@
 // 本页面随意修改
 import React, { Component } from 'react'
-import { Input } from 'antd';
-export default class InputNameComponent extends Component {
+import {
+  Upload, message, Button, Icon,
+} from 'antd';
+
+const props = {
+  name: 'file',
+  action: '//jsonplaceholder.typicode.com/posts/',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file.name);  // 文件名
+      // console.log( info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+export default class OtherdocumentsComponent extends Component {
   render() {
     return (
       <div>
-        <Input placeholder="sdadas" />
+        <Upload {...props}>
+              <Button>
+                  <Icon type="upload" /> 上传文件
+              </Button>
+        </Upload>
       </div>
     )
   }
