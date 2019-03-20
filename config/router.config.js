d951256575c3331abe8f20969bd1dc4755b4bfdf
Why-WU <wuhanyu001@126.com>
2019-03-20 09:36:51 +0800

    add

--- config/router.config.js
+++ config/router.config.js
@@ -20,19 +20,32 @@ export default [
     component: '../layouts/BasicLayout',
     Routes: ['src/pages/Authorized'],
     routes: [
-
       {
         path: '/project', //一级目录
         icon: 'project',
         name: 'project',
         routes: [
           {
-            path: '/project/add',//二级目录
+            path: '/project/add', //二级目录
             name: 'add',
             icon: 'setting',
             component: './project/add',
           },
-          
+        ],
+      },
+
+      //员工管理oa
+      {
+        path: '/worker',
+        icon: 'project',
+        name: 'worker',
+        routes: [
+          {
+            path: '/worker/addmodification',
+            name: 'addmodification',
+            icon: 'setting',
+            component: './worker/addmodification',
+          },
         ],
       },
       {
---
+++ src/pages/Worker/AddModification.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import FormList from '@/pages/FormList/FormList';
+
+export default class AddModification extends Component {
+  render() {
+    return (
+      <div>
+        <FormList />
+      </div>
+    );
+  }
+}
