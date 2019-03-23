83c277b69fc4cb1e3f67412f77a14a74a0a81638
tyler <1732749825@qq.com>
2019-03-23 12:27:07 +0800

    lxj

--- src/components/AddComponent/ProjectType.js
+++ src/components/AddComponent/ProjectType.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import InputSelectComponent from '@/components/AddComponent/InputSelectComponent';
 import AddComponent from '@/components/AddComponent/AddComponent'
-import ChangeComponent from '@/components/AddComponent/ChangeComponent'
 export default class ProjectType extends Component {
   constructor(props){
     super(props);
