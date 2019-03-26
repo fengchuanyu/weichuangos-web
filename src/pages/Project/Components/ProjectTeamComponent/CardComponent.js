2b11df296bd2be3380675023fc53910c84b32641
tyler <1732749825@qq.com>
2019-03-26 16:25:55 +0800

    lxj

--- src/pages/Project/Components/ProjectTeamComponent/CardComponent.js
+++ src/pages/Project/Components/ProjectTeamComponent/CardComponent.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
-import { Card, Col, Row ,Avatar,Icon,List,Button,Modal} from 'antd';
+import { Card, Col, Row ,Avatar,Icon,List,Button,Modal,Select,Input} from 'antd';
 import styles from './Card.less'
-const data=[{key:"组长:",value:"ehrthbt"},{key:"组员:",value:"htrhtr"},{key:"组内人数:",value:"htrhtr"},{key:"报选项目:",value:"htrhtr"},{key:"项目报选时间:",value:"htrhtr"}]
-const data1=[{key:"组长:",value:"gegvgrtn"},{key:"组员:",value:"gegvgrtn"},{key:"组内人数:",value:"gegvgrtn"},{key:"报选项目:",value:"gegvgrtn"},{key:"项目报选时间:",value:"gegvgrtn"}]
-const data2=[{key:"组长:",value:"ukiumiukmu"},{key:"组员:",value:"ukiumiukmu"},{key:"组内人数:",value:"ukiumiukmu"},{key:"报选项目:",value:"ukiumiukmu"},{key:"项目报选时间:",value:"ukiumiukmu"}]
-const datas=[
-  {id:1,data:data},{id:2,data:data1},{id:3,data:data2},{id:4,data:data},{id:5,data:data},{id:6,data:data},{id:7,data:data},{id:8,data:data},{id:9,data:data}
-]
-const list=[1,2,3,4,5,6,7,8,9]
+const Search = Input.Search;
 export default class CardComponent extends Component {
   constructor(props){
     super(props);
     this.state={
       visible: false,
-      nowData:[]
+      nowData:[],
+      list:[],
+      datas:[],
+      nowDatas:[],
     }
+  }
+  componentDidMount() {
+    this.setState({
+      list: this.props.List.concat({ title: '全部项目', id: this.props.List.length, isActive: false}),
+      datas:this.props.Datas,
+      nowDatas:this.props.Datas
+    });
   }
   showModal = (data) => {
     console.log(data);
@@ -37,45 +41,95 @@ export default class CardComponent extends Component {
       visible: false,
     });
   }
+  handleChange(value) {
+    if(value!="全部项目"){
+      this.setState({
+        nowDatas:this.state.datas.filter((filterData)=>{
+          return filterData.data[3].value==value;
+        })
+      })
+    }else{
+      this.setState({
+        nowDatas:this.state.datas
+      })
+    }
+    }
   render() {
+    const children = [];
+    for (let i = 0; i < this.state.list.length; i++) {
+      children.push(<Option key={this.state.list[i].id} value={this.state.list[i].title}>{this.state.list[i].title}</Option>);
+    }
     return (
       <div>
-        <div style={{ background: '#ECECEC', padding: '30px' }}>
-        <List
-          size="small"
-          bordered
-          style={{height: 930}}
-          dataSource={datas}
-          renderItem={data=> (
-          <List.Item key={data.id} className={styles.card}>
-            <Card
-              style={{ width: 260,marginLeft:15,marginRight:15 }}
-              actions={[<Button type="primary" onClick={this.showModal.bind(this,data.data)}>查看详情</Button>]}
+        <span>
+          <span>
+            <span style={{ marginLeft: 390 }}>进行中的项目：</span>
+            <Select defaultValue={this.state.nowValue?this.state.nowValue:"全部项目" }
+            style={{ width: 300 }} 
+            onChange={this.handleChange.bind(this)}
             >
-              <List
-                size="small"
-                bordered
-                dataSource={data.data}
-                renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
-              />
-            </Card>
-          </List.Item>
-          )}
-        />
-      </div>
-      <Modal
-          title="Basic Modal"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <List
-                size="small"
-                bordered
-                dataSource={this.state.nowData}
-                renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
+              {children}
+            </Select>
+          </span>
+          <span>
+            <Search
+              placeholder="请输入"
+              onSearch={value => {
+              {if(value!=""){
+                  this.setState({
+                    nowDatas:this.state.datas.filter((filterData)=>{
+                      return filterData.data[3].value.indexOf(value)!=-1;
+                    })
+                  })
+                }else{
+                  this.setState({
+                    nowDatas:this.state.nowDatas
+                  })
+                }
+              }
+          }
+          }
+              enterButton
+              style={{ width: 300, marginLeft: 90 }}
             />
-        </Modal>
+          </span>
+        </span>
+        <div style={{ background: '#ECECEC', padding: '30px' }}>
+          <List
+            size="small"
+            bordered
+            style={{height: 930}}
+            dataSource={this.state.nowDatas}
+            renderItem={data=> (
+            <List.Item key={data.id} className={styles.card}>
+              <Card
+                style={{ width: 260,marginLeft:15,marginRight:15 }}
+                actions={[<Button type="primary" onClick={this.showModal.bind(this,data.data)}>查看详情</Button>]}
+              >
+                <List
+                  size="small"
+                  bordered
+                  dataSource={data.data}
+                  renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
+                />
+              </Card>
+            </List.Item>
+            )}
+          />
+        </div>
+        <Modal
+            title="项目详细信息"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <List
+                  size="small"
+                  bordered
+                  dataSource={this.state.nowData}
+                  renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
+              />
+          </Modal>
       </div>
     )
   }
--- src/pages/Project/ProjectTeam.js
+++ src/pages/Project/ProjectTeam.js
@@ -1,16 +1,27 @@
 //项目组页面-------------------------------------------------------------------------
 import React, { Component } from 'react';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper'; //面包屑
-import SelectComponent from './components/ProjectTeamComponent/SelectComponent'; //项目选择
 import CardComponent from './components/ProjectTeamComponent/CardComponent'; //栅格卡片
-
+const data=[{key:"组长:",value:"ehrthbt"},{key:"组员:",value:"htrhtr"},{key:"组内人数:",value:"htrhtr"},{key:"报选项目:",value:"微信小程序"},{key:"项目报选时间:",value:"htrhtr"}];
+const data1=[{key:"组长:",value:"gegvgrtn"},{key:"组员:",value:"gegvgrtn"},{key:"组内人数:",value:"gegvgrtn"},{key:"报选项目:",value:"移动端开发"},{key:"项目报选时间:",value:"gegvgrtn"}];
+const data2=[{key:"组长:",value:"ukiumiukmu"},{key:"组员:",value:"ukiumiukmu"},{key:"组内人数:",value:"ukiumiukmu"},{key:"报选项目:",value:"Vue"},{key:"项目报选时间:",value:"ukiumiukmu"}];
+const datas=[
+  {id:1,data:data},{id:2,data:data1},{id:3,data:data2},{id:4,data:data},{id:5,data:data},{id:6,data:data},{id:7,data:data},{id:8,data:data},{id:9,data:data}
+];
+const list=[{ title: '微信小程序', id: 1, isActive: false },{ title: '移动端开发', id: 2, isActive: false },{ title: 'Vue', id: 3, isActive: false }];
 export default class ProjectTeam extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      datas:datas,
+      list:list
+    }
+  }
   render() {
     return (
       <PageHeaderWrapper title="项目组">
         <div>
-          <SelectComponent />
-          <CardComponent />
+          <CardComponent Datas={this.state.datas} List={this.state.list}/>
         </div>
       </PageHeaderWrapper>
     );
