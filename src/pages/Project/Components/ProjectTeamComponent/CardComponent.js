932acc55ddba0f13326a86918a905b458bf2f664
tyler <1732749825@qq.com>
2019-03-26 12:13:01 +0800

    lxj

--- src/pages/Project/Components/ProjectTeamComponent/CardComponent.js
+++ src/pages/Project/Components/ProjectTeamComponent/CardComponent.js
@@ -1,104 +1,83 @@
-import React, { Component } from 'react';
-import { Card, Col, Row, Avatar, Icon, List } from 'antd';
-import ModalComponent from './ModalComponent';
-import styles from './Card.less';
-const data = [
-  { key: '组名', value: '' },
-  { key: '组长', value: '' },
-  { key: '组员', value: '' },
-  { key: '组内人数', value: '' },
-  { key: '报选项目', value: '' },
-  { key: '项目报选时间', value: '' },
-];
-
-const data1 = [
-  { key: '组名', value: '' },
-  { key: '组长', value: '' },
-  { key: '组员', value: '' },
-  { key: '组内人数', value: '' },
-  { key: '报选项目', value: '' },
-  { key: '项目报选时间', value: '' },
-];
-
-const data2 = [
-  { key: '组名', value: '' },
-  { key: '组长', value: '' },
-  { key: '组员', value: '' },
-  { key: '组内人数', value: '' },
-  { key: '报选项目', value: '' },
-  { key: '项目报选时间', value: '' },
-];
-
-const datas = [
-  { id: 1, data: data },
-  { id: 2, data: data1 },
-  { id: 3, data: data2 },
-  { id: 4, data: data },
-  { id: 5, data: data },
-  { id: 6, data: data },
-  { id: 7, data: data },
-  { id: 8, data: data },
-  { id: 9, data: data },
-];
-const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import React, { Component } from 'react'
+import { Card, Col, Row ,Avatar,Icon,List,Button,Modal} from 'antd';
+import styles from './Card.less'
+const data=[{key:"组长:",value:"ehrthbt"},{key:"组员:",value:"htrhtr"},{key:"组内人数:",value:"htrhtr"},{key:"报选项目:",value:"htrhtr"},{key:"项目报选时间:",value:"htrhtr"}]
+const data1=[{key:"组长:",value:"gegvgrtn"},{key:"组员:",value:"gegvgrtn"},{key:"组内人数:",value:"gegvgrtn"},{key:"报选项目:",value:"gegvgrtn"},{key:"项目报选时间:",value:"gegvgrtn"}]
+const data2=[{key:"组长:",value:"ukiumiukmu"},{key:"组员:",value:"ukiumiukmu"},{key:"组内人数:",value:"ukiumiukmu"},{key:"报选项目:",value:"ukiumiukmu"},{key:"项目报选时间:",value:"ukiumiukmu"}]
+const datas=[
+  {id:1,data:data},{id:2,data:data1},{id:3,data:data2},{id:4,data:data},{id:5,data:data},{id:6,data:data},{id:7,data:data},{id:8,data:data},{id:9,data:data}
+]
+const list=[1,2,3,4,5,6,7,8,9]
 export default class CardComponent extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = {
-      nowData: [],
-    };
+    this.state={
+      visible: false,
+      nowData:[]
+    }
   }
-  content(data) {
-    // console.log(123);
-
+  showModal = (data) => {
+    console.log(data);
+    
     this.setState({
-      nowData: data,
+      visible: true,
+      nowData:data
+    });
+  }
+
+  handleOk = (e) => {
+    this.setState({
+      visible: false,
+      
+    });
+  }
+
+  handleCancel = (e) => {
+    this.setState({
+      visible: false,
     });
   }
   render() {
-    const { Meta } = Card;
     return (
       <div>
-        <div style={{ background: '#ECECEC', padding: '5px' }}>
-          <List
-            size="small"
-            bordered={{ width: 1100 }}
-            style={{ height: 1050, width: 1250 }}
-            dataSource={datas}
-            renderItem={data => (
-              <List.Item key={data.id} className={styles.card}>
-                <Card
-                  style={{
-                    height: 320,
-                    width: 363,
-                    marginLeft: 1,
-                    marginRight: 1,
-                    marginBottom: 10,
-                  }}
-                  actions={[
-                    <ModalComponent
-                      NowData={this.state.nowData}
-                      onClick={this.content.bind(this, data)}
-                    />,
-                  ]}
-                >
-                  <List
-                    size="small"
-                    bordered={{ width: 400 }}
-                    dataSource={data.data}
-                    renderItem={item => (
-                      <List.Item key={item.key}>
-                        {item.key}:{item.value}
-                      </List.Item>
-                    )}
-                  />
-                </Card>
-              </List.Item>
-            )}
-          />
-        </div>
-        ,
+        <div style={{ background: '#ECECEC', padding: '30px' }}>
+        <List
+          size="small"
+          bordered
+          style={{height: 930}}
+          dataSource={datas}
+          renderItem={data=> (
+          <List.Item key={data.id} className={styles.card}>
+            <Card
+              style={{ width: 260,marginLeft:15,marginRight:15 }}
+              actions={[<Button type="primary" onClick={this.showModal.bind(this,data.data)}>查看详情</Button>]}
+            >
+              <List
+                size="small"
+                bordered
+                dataSource={data.data}
+                renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
+              />
+            </Card>
+          </List.Item>
+          )}
+        />
       </div>
-    );
+      <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <List
+                size="small"
+                bordered
+                dataSource={this.state.nowData}
+                renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
+            />
+        </Modal>
+      </div>
+    )
   }
 }
+
