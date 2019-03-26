61ae11ce747ee32285a7592be43baecd3f07b86a
chenzhiwei0109 <1138476755@qq.com>
2019-03-26 11:54:20 +0800

    commit

--- src/pages/Project/Components/ProjectTeamComponent/CardComponent.js
+++ src/pages/Project/Components/ProjectTeamComponent/CardComponent.js
@@ -1,65 +1,104 @@
-import React, { Component } from 'react'
-import { Card, Col, Row ,Avatar,Icon,List} from 'antd';
-import ModalComponent from './ModalComponent'
-import styles from './Card.less'
-const data=[{key:"组名",value:""},{key:"组长",value:""},{key:"组员",value:""},
-{key:"组内人数",value:""},{key:"报选项目",value:""},{key:"项目报选时间",value:""}]
+import React, { Component } from 'react';
+import { Card, Col, Row, Avatar, Icon, List } from 'antd';
+import ModalComponent from './ModalComponent';
+import styles from './Card.less';
+const data = [
+  { key: '组名', value: '' },
+  { key: '组长', value: '' },
+  { key: '组员', value: '' },
+  { key: '组内人数', value: '' },
+  { key: '报选项目', value: '' },
+  { key: '项目报选时间', value: '' },
+];
 
-const data1=[{key:"组名",value:""},{key:"组长",value:""},{key:"组员",value:""},
-{key:"组内人数",value:""},{key:"报选项目",value:""},{key:"项目报选时间",value:""}]
+const data1 = [
+  { key: '组名', value: '' },
+  { key: '组长', value: '' },
+  { key: '组员', value: '' },
+  { key: '组内人数', value: '' },
+  { key: '报选项目', value: '' },
+  { key: '项目报选时间', value: '' },
+];
 
-const data2=[{key:"组名",value:""},{key:"组长",value:""},{key:"组员",value:""},
-{key:"组内人数",value:""},{key:"报选项目",value:""},{key:"项目报选时间",value:""}]
+const data2 = [
+  { key: '组名', value: '' },
+  { key: '组长', value: '' },
+  { key: '组员', value: '' },
+  { key: '组内人数', value: '' },
+  { key: '报选项目', value: '' },
+  { key: '项目报选时间', value: '' },
+];
 
-const datas=[
-  {id:1,data:data},{id:2,data:data1},{id:3,data:data2},{id:4,data:data},{id:5,data:data},{id:6,data:data},{id:7,data:data},{id:8,data:data},{id:9,data:data}
-]
-const list=[1,2,3,4,5,6,7,8,9]
+const datas = [
+  { id: 1, data: data },
+  { id: 2, data: data1 },
+  { id: 3, data: data2 },
+  { id: 4, data: data },
+  { id: 5, data: data },
+  { id: 6, data: data },
+  { id: 7, data: data },
+  { id: 8, data: data },
+  { id: 9, data: data },
+];
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 export default class CardComponent extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      nowData:[]
-    }
+    this.state = {
+      nowData: [],
+    };
   }
-  content(data){
+  content(data) {
     // console.log(123);
-    
+
     this.setState({
-      nowData:data
-    })
+      nowData: data,
+    });
   }
   render() {
     const { Meta } = Card;
     return (
       <div>
         <div style={{ background: '#ECECEC', padding: '5px' }}>
-            <List
+          <List
             size="small"
-            bordered={{width:1100}}
-            style={{height: 1050, width:1250}}
+            bordered={{ width: 1100 }}
+            style={{ height: 1050, width: 1250 }}
             dataSource={datas}
-            renderItem={data=> (
-            <List.Item key={data.id} className={styles.card}>
-              <Card
-                style={{height:320,width: 363,marginLeft:1,marginRight:1,marginBottom:10}}
-                actions={[<ModalComponent  NowData={this.state.nowData} onClick={this.content.bind(this,data)}/>]}
-              >
-                <List
-                  size="small"
-                  bordered={{width:400}}
-                  dataSource={data.data}
-                  renderItem={item => (<List.Item key={item.key}>{item.key}:{item.value}</List.Item>)}
-                />
-              </Card>
-            </List.Item>
+            renderItem={data => (
+              <List.Item key={data.id} className={styles.card}>
+                <Card
+                  style={{
+                    height: 320,
+                    width: 363,
+                    marginLeft: 1,
+                    marginRight: 1,
+                    marginBottom: 10,
+                  }}
+                  actions={[
+                    <ModalComponent
+                      NowData={this.state.nowData}
+                      onClick={this.content.bind(this, data)}
+                    />,
+                  ]}
+                >
+                  <List
+                    size="small"
+                    bordered={{ width: 400 }}
+                    dataSource={data.data}
+                    renderItem={item => (
+                      <List.Item key={item.key}>
+                        {item.key}:{item.value}
+                      </List.Item>
+                    )}
+                  />
+                </Card>
+              </List.Item>
             )}
           />
-      
-       
-
-  </div>,
+        </div>
+        ,
       </div>
-    )
+    );
   }
 }
--- src/pages/Project/Components/ProjectTeamComponent/SelectComponent.js
+++ src/pages/Project/Components/ProjectTeamComponent/SelectComponent.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Select,Input } from 'antd';
+import React, { Component } from 'react';
+import { Select, Input } from 'antd';
 
 const Option = Select.Option;
 const Search = Input.Search;
@@ -12,25 +12,24 @@ export default class SelectComponent extends Component {
       <div>
         <span>
           <span>
-              <span style={{marginLeft:390}}>进行中的项目：</span>
-              <Select defaultValue="lucy" style={{ width: 300 }} onChange={handleChange}>
+            <span style={{ marginLeft: 390 }}>进行中的项目：</span>
+            <Select defaultValue="lucy" style={{ width: 300 }} onChange={handleChange}>
               <Option value="jack">正在进行的项目1</Option>
               <Option value="lucy">正在进行的项目2</Option>
-              <Option value="disabled" >正在进行的项目3</Option>
+              <Option value="disabled">正在进行的项目3</Option>
               <Option value="Yiminghe">正在进行的项目4</Option>
-              </Select>
+            </Select>
           </span>
           <span>
-          <Search
-            placeholder="请输入"
-            onSearch={value => console.log(value)}
-            enterButton
-            style={{width:300 ,marginLeft:90}}
-          />
+            <Search
+              placeholder="请输入"
+              onSearch={value => console.log(value)}
+              enterButton
+              style={{ width: 300, marginLeft: 90 }}
+            />
           </span>
         </span>
-         
       </div>
-    )
+    );
   }
 }
