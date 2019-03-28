dd9c7593b3c13e542472dcc5fea22aa4fa2546e0
chenzhiwei0109 <1138476755@qq.com>
2019-03-28 11:43:14 +0800

    'commit'

--- src/pages/Project/Add.js
+++ src/pages/Project/Add.js
@@ -1,9 +1,10 @@
 // 项目编辑页面-------------------------------------------------------------------------
 import React, { Component } from 'react';
+import { Card } from 'antd';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper'; //面包屑
 import InputNameComponent from './components/AddComponent/InputNameComponent'; //项目名称
 import ProjectType from './components/AddComponent/ProjectType'; //项目分类
-import InputNumberComponent from './components/AddComponent/InputNumberComponent'; //项目编号 
+import InputNumberComponent from './components/AddComponent/InputNumberComponent'; //项目编号
 import SelectComponent from './components/AddComponent/SelectComponent'; //技术栈
 import OtherdocumentsComponent from './components/AddComponent/OtherdocumentsComponent'; //上传文件
 import ProgressIntroductionConponent from './components/AddComponent/ProgressIntroductionConponent'; //项目介绍
@@ -17,16 +18,18 @@ export default class Add extends Component {
   render() {
     return (
       <PageHeaderWrapper title="项目编辑">
-        <div>
-          {/* <Button onClick={this.click.bind(this)}>OK</Button> */}
-          <InputNameComponent />
-          <ProjectType />
-          <InputNumberComponent />
-          <SelectComponent />
-          <OtherdocumentsComponent />
-          <ProgressIntroductionConponent />
-          <ButtonComponent />
-        </div>
+        <Card bordered={false}>
+          <div>
+            {/* <Button onClick={this.click.bind(this)}>OK</Button> */}
+            <InputNameComponent />
+            <ProjectType />
+            <InputNumberComponent />
+            <SelectComponent />
+            <OtherdocumentsComponent />
+            <ProgressIntroductionConponent />
+            <ButtonComponent />
+          </div>
+        </Card>
       </PageHeaderWrapper>
     );
   }
